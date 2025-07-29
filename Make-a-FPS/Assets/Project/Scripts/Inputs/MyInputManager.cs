85b3e7304be8335f6e134249e33dae4cfb830c79
eiji-miyazawa <kenji.ver2.0@gmail.com>
2025-07-29 21:35:36 +0900

    今回専用のInputActionを作成（デフォルトは削除）

---
+++ Make-a-FPS/Assets/Project/Scripts/Inputs/MyInputManager.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace foRCreative.App.MakeAFps.Project.Scripts.Inputs
+{
+    public class MyInputManager : MonoBehaviour
+    {
+        PlayerInput  _playerInput;
+        
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
+    }
+}
