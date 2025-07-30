40a0c797faba7f38136365f80f3c490056a23c8c
eiji-miyazawa <kenji.ver2.0@gmail.com>
2025-07-30 21:09:12 +0900

    InputManagerにMoveの入力を返すプロパティ追加

--- Make-a-FPS/Assets/Project/Scripts/Inputs/MyInputManager.cs
+++ Make-a-FPS/Assets/Project/Scripts/Inputs/MyInputManager.cs
@@ -1,22 +1,54 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 namespace foRCreative.App.MakeAFps.Project.Scripts.Inputs
 {
+    [RequireComponent(typeof(PlayerInput))]
     public class MyInputManager : MonoBehaviour
     {
-        PlayerInput  _playerInput;
+        private PlayerInput _playerInput;
         
-        // Start is called once before the first execution of Update after the MonoBehaviour is created
-        void Start()
+        //  Moveの入力用
+        private Vector2 _moveInput;
+        
+        /// <summary>
+        /// 読み取り専用 Moveの入力を返す
+        /// </summary>
+        public Vector2 MoveInput => _moveInput;
+        
+        private void OnMove(InputAction.CallbackContext obj)
         {
-            
+            switch (obj.phase)
+            {
+                case InputActionPhase.Performed:
+                    _moveInput = obj.ReadValue<Vector2>();
+                    break;
+                case InputActionPhase.Canceled:
+                    _moveInput = Vector2.zero;
+                    break;
+            }
         }
 
-        // Update is called once per frame
-        void Update()
+        private void Awake()
         {
-            
+            _playerInput = GetComponent<PlayerInput>();
+        }
+
+        private void OnEnable()
+        {
+            if(_playerInput == null) return;
+            //  デリケート登録
+            _playerInput.actions["Move"].performed += OnMove;
+            _playerInput.actions["Move"].canceled += OnMove;
+        }
+
+        private void OnDisable()
+        {
+            if (_playerInput == null) return;
+            //  デリケート登録解除
+            _playerInput.actions["Move"].performed -= OnMove;
+            _playerInput.actions["Move"].canceled -= OnMove;
         }
     }
 }
