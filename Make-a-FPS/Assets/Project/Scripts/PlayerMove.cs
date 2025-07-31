6f7e07d29390162988bc5903089352316379974c
eiji-miyazawa <kenji.ver2.0@gmail.com>
2025-07-31 21:01:36 +0900

    Cinemachine導入 移動系開発用のシーン作成(床を設置)

---
+++ Make-a-FPS/Assets/Project/Scripts/PlayerMove.cs
@@ -0,0 +1,24 @@
+using foRCreative.App.MakeAFps.Project.Scripts.Inputs;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace foRCreative.App.MakeAFps.Project.Scripts
+{
+    public class PlayerMove : MonoBehaviour
+    {
+        [SerializeField] private MyInputManager inputManager;
+        
+        
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
+    }
+}
