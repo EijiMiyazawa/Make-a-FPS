a210c988fe25e936030e0339564ac0faf788ca11
eiji-miyazawa <kenji.ver2.0@gmail.com>
2025-08-01 21:16:39 +0900

    移動処理実装

--- Make-a-FPS/Assets/Project/Scripts/PlayerMove.cs
+++ Make-a-FPS/Assets/Project/Scripts/PlayerMove.cs
@@ -7,18 +7,24 @@ namespace foRCreative.App.MakeAFps.Project.Scripts
     public class PlayerMove : MonoBehaviour
     {
         [SerializeField] private MyInputManager inputManager;
-        
+        [SerializeField] private float moveSpeed = 2.0f;
+        private Transform _playerTransform;
         
         // Start is called once before the first execution of Update after the MonoBehaviour is created
         void Start()
         {
-            
+            //  Transformをキャッシュ
+            _playerTransform = transform;
         }
 
         // Update is called once per frame
         void Update()
         {
+            //  入力から移動方向を取得
+            Vector3 moveDirection = new Vector3(inputManager.MoveInput.x, 0f, inputManager.MoveInput.y);
             
+            //  プレイヤー移動
+            _playerTransform.position +=  Time.deltaTime * moveSpeed  * moveDirection;
         }
     }
 }
