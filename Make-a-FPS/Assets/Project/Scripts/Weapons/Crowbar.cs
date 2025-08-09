cc3315d491576ec91caca45efe10400fd4e2f6d1
eiji-miyazawa <kenji.ver2.0@gmail.com>
2025-08-09 15:58:26 +0900

    Fireを押した瞬間のみ攻撃するように修正(簡易的なステートパターン実装)

--- Make-a-FPS/Assets/Project/Scripts/Weapons/Crowbar.cs
+++ Make-a-FPS/Assets/Project/Scripts/Weapons/Crowbar.cs
@@ -4,11 +4,35 @@ using UnityEngine.InputSystem;
 
 namespace foRCreative.App.MakeAFps.Project.Scripts.Weapons
 {
+    public enum CrowbarState
+    {
+        Idle = 0,
+        Attacking = 1
+    }
+    
     public class Crowbar : Weapon
     {
+        private CrowbarState _state;
+
+        private void Start()
+        {
+            _state = CrowbarState.Idle;
+        }
+        
         public override void Attack(bool isFire = false)
         {
-            WeaponAnimator.SetBool("IsAttacking", isFire);
+            if (isFire)
+            {
+                if (_state == CrowbarState.Idle)
+                {
+                    _state = CrowbarState.Attacking;
+                    weaponAnimator.SetTrigger("Attack");
+                }
+            }
+            else
+            {
+                _state = CrowbarState.Idle;
+            }
         }
     }
 }
--- Make-a-FPS/Assets/Project/Scripts/Weapons/Weapon.cs
+++ Make-a-FPS/Assets/Project/Scripts/Weapons/Weapon.cs
@@ -8,17 +8,7 @@ namespace foRCreative.App.MakeAFps.Project.Scripts.Weapons
         /// <summary>
         /// 読み取り専用(start時自動割り当て)
         /// </summary>
-        public Animator WeaponAnimator { get; private set; }
-
-        // Start is called once before the first execution of Update after the MonoBehaviour is created
-        void Start()
-        {
-            WeaponAnimator  = GetComponent<Animator>();
-            if (WeaponAnimator == null)
-            {
-                Debug.LogError("This Weapon not attached Animator");
-            }
-        }
+        [SerializeField] protected Animator weaponAnimator;
 
         /// <summary>
         /// 武器で攻撃
